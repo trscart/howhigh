2a6e62ad4ca6fb1ef0093e6c7f40bcd5a79cc651
trscart <umbertotruscello1998@gmail.com>
2020-11-25 23:29:03 +0100

    gif, buttons

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Hello Developer")
 
-    $("form").submit(function(){
+    $("form").submit(function () {
         fbq('track', 'CompleteRegistration');
     })
 
     // animazione navbar scroll
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 0) { // nav-item change on scroll
+        if ($(window).scrollTop() > 0) {
             $('.hh_navbar').addClass("hh_navbar_scrolled")
         } else {
             $('.hh_navbar').removeClass("hh_navbar_scrolled")
         }
     });
+
+    // animazione banner
+    let not_scrolled = true
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 800 && not_scrolled) {
+            $('.hh_banner').css("bottom", "0")
+            not_scrolled = false
+        }
+    });
+    $("#hh_close_banner").click(function () {
+        $('.hh_banner').css("bottom", "-200px")
+    })
 })
